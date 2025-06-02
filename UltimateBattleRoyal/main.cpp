fc5c781e86583785740442a3b82570fe4d62b206
LFMineiro <lflmalbuquerque3@gmail.com>
2025-06-02 20:53:21 -0300

    fix(confrontation): closes #1 use dynamic_cast to check if the loser is a Villain

--- UltimateBattleRoyal/main.cpp
+++ UltimateBattleRoyal/main.cpp
@@ -57,9 +57,12 @@ class Villain : public Person {
     public:
         Villain(string n): Person(n) {}
 
-        int getTotalPower(){;
+        int getTotalPower(){
             return (Person::getTotalPower() * (1 + (0.04 * jailTime)));
             // Villains have 4% more power for each year in jail
+        }
+        int getJailTime() {
+            return jailTime;
         }
         void setJailTime(int time) {
             jailTime = time;
@@ -68,22 +71,33 @@ class Villain : public Person {
 
 class Confrontation {
     public:
-        void fight(Hero& hero, Villain& villain) {
-            int heroPower = hero.getTotalPower();
-            int villainPower = villain.getTotalPower();
+        void fight(Person& p1, Person& p2) {
+            int p1Power = p1.getTotalPower();
+            int p2Power = p2.getTotalPower();
 
-            cout << "The Hero " << hero.getName() << " with power " << hero.getTotalPower() << " fight against the Villain " << villain.getName() << " with power " << endl;
+            Villain* v1 = dynamic_cast<Villain*>(&p1);
+            Villain* v2 = dynamic_cast<Villain*>(&p2);
 
-            if(heroPower > villainPower) {
-                cout << hero.getName() << " wins with power: " << heroPower << endl;
-            } else if(villainPower > heroPower) {
-                cout << villain.getName() << " wins with power: " << villainPower << endl;
-            } else {
-                cout << "It's a tie!" << endl;
+            if(p1Power > p2Power) {
+                if(v2) {
+                    cout << p1.getName() << " wins against " << p2.getName() << endl;
+                    v2->setJailTime(v2->getJailTime()  + 2);
+                }
+                else {
+                    cout << p1.getName() << " wins against " << p2.getName() << endl;
+                }
             }
-        }
+            else if(p1Power < p2Power) {
+                if(v1) {
+                    cout << p2.getName() << " wins against " << p1.getName() << endl;
+                    v1->setJailTime(v1->getJailTime() + 2);
+                }
+                else {
+                    cout << p2.getName() << " wins against " << p1.getName() << endl;
+                }
+            }
+        }         
 };
-
 
 int main() {
     SuperPower flight("Flight", 100);
@@ -100,6 +114,7 @@ int main() {
 
     Confrontation confrotation;
     confrotation.fight(hero, villain);
-    
+    cout << "Lex Luthor has " << villain.getJailTime() << " years in Jail" << endl;
 
+    return 0;
 }
